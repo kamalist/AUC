1205e105031019c464d76f4511652536af6126bd
kamalist <kamalist5@gmail.com>
2019-01-07 15:20:15 +0400

    -d parameter added. Now it's possible to specify own date to print

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # AUC
-**AUC** (Ab Urbe condita) is a command-line Roman calendar tool. Currently it shows the current date in the format of the Ancient Romans. 
+**AUC** (Ab Urbe condita) is a command-line Roman calendar tool. Currently it shows the specified date in the format of the Ancient Romans. 
+
+# Synopsis
+auc [-d d/m/y]
+
+# Parameters
+Optional parameter -d allows to specify a date to print in the Roman format (the current date is printed by default). The only supported format for date is d/m/y, d and m could have leading zeros but it is not required. Examples: 1/1/2019, 01/01/2019.
 
 # Output details
 The months' names are from the Empire ages (i.e. Quintilis called Iulius and Sextilis called Augustus). 
@@ -12,4 +18,20 @@ The tool makes only formatting in the Ancient Romans' style, usual Gregorian cal
   $ ./auc
 
   Non. Ian., MMDCCLXXII AUC
+
+  $ ./auc -d 24/2/2000
+  
+  a.d. VI Kal. Mar., MMDCCLIII AUC
+
+  $ ./auc -d 25/2/2000
+  
+  a.d. VI Kal. Mar., MMDCCLIII AUC
+
+  $ ./auc -d 24/2/2001
+
+  a.d. VI Kal. Mar., MMDCCLIV AUC
+
+  $ ./auc -d 25/2/2001
+  
+  a.d. V Kal. Mar., MMDCCLIV AUC
 
--- main.c
+++ main.c
@@ -1,29 +1,73 @@
+#define _XOPEN_SOURCE
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
+
 #include "roman_date.h"
 #include "roman_numerals.h"
 
-int main (int argc, char * argv[]){
-	time_t t = time (NULL);
-	struct tm t_struct = *localtime (&t);
-	int d = t_struct.tm_mday;
-	int m = t_struct.tm_mon;
-	int y = t_struct.tm_year + 1900;
+struct conf {
+	bool cur_time;
+	struct tm t_struct; //not used if cur_time = true
+};
+
+void parse_args (struct conf * config, int argc, char * argv[]) {
+	int c;
+	char * tmp;
+	while ((c = getopt (argc, argv, "d:")) != -1) {
+		switch (c){
+			case 'd':
+				tmp = strptime (optarg, "%d/%m/%Y", &config->t_struct);
+				if (tmp == NULL || *tmp != 0) {
+					fprintf (stderr, "%s is invalid data\n", optarg);
+					exit (EXIT_FAILURE);
+				}
+				config->cur_time = false;
+				break;
+		}
+	}
+}
+
+void fill_date (struct conf * c) {
+	if (c->cur_time) {
+		time_t t = time (NULL);
+		c->t_struct = *localtime (&t);
+	}
+}
+
+void print_day (struct conf * c) {
+	int d = c->t_struct.tm_mday;
+	int m = c->t_struct.tm_mon;
+	int y = c->t_struct.tm_year + 1900;
 	char buf[100];
 	if (roman_day (d, m, y, buf, 100)){
 		printf ("%s, ", buf);
-		if (roman_numeral (y + 753, buf, 100) > 0) {
-			printf ("%s AUC\n", buf);
-		}
-		else {
-			printf ("year unrecognized (val. %d)\n", y);
-			return EXIT_FAILURE;
-		}
+	} else {
+		printf ("Date %d/%d/%d is invalid\n", d, m, y);
+		exit (EXIT_FAILURE);
+	}
+}
+
+void print_year (struct conf * c) {
+	int y = c->t_struct.tm_year + 1900;
+	char buf[100];
+	if (roman_numeral (y + 753, buf, 100) > 0) {
+		printf ("%s AUC\n", buf);
 	}
 	else {
-		printf ("Date %d/%d/%d is invalid\n", d, m, y);
-		return EXIT_FAILURE;
+		printf ("year unrecognized (val. %d)\n", y);
+		exit (EXIT_FAILURE);
 	}
+}
+int main (int argc, char * argv[]){
+	struct conf config = {
+		.cur_time = true,
+	};
+	parse_args (&config, argc, argv);
+	fill_date (&config);
+	print_day (&config);
+	print_year (&config);
 	return EXIT_SUCCESS;
 }
